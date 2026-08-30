0a45ae08ddfca80d0b8dcb80ac025bb9b19990a8
Filipe Castro Fernandes <filipecfernandes2004@gmail.com>
2026-08-30 01:20:06 -0300

    refactor: remove redundant Django app in favor of GitHub Pages and lightweight local server

--- web_app/urls.py
+++
@@ -1,9 +0,0 @@
-"""URL configuration for web_app project."""
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("dashboard_ui.urls")),
-]
